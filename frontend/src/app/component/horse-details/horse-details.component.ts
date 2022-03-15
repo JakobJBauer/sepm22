464855af6deb65c947f33d628e64c48bafd3ea97
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2022-03-15 18:56:38 +0100

    [US6] Added delete button to horse detail component and added error view

--- frontend/src/app/component/horse-details/horse-details.component.ts
+++ frontend/src/app/component/horse-details/horse-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Horse} from '../../dto/horse';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {HorseService} from '../../service/horse.service';
 
 @Component({
@@ -15,6 +15,7 @@ export class HorseDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private service: HorseService
   ) { }
 
@@ -25,9 +26,29 @@ export class HorseDetailsComponent implements OnInit {
           this.horse = horse;
         },
         error: error => {
-          this.error = error.message;
+          this.showError('Could not load horse\n' + error.message, 0);
         }
       });
     });
+  }
+
+  deleteHorseById(id: number): void {
+    this.service.deleteById(id).subscribe({
+      next: () => {
+        this.router.navigate(['/horses']);
+      },
+      error: error => {
+        this.showError('Could not delete horse\n' + error.message);
+      }
+    });
+  }
+
+  private showError(error: string, timeout: number = 10000): void {
+    this.error = error;
+    if (timeout !== 0) {
+      setTimeout(() => {
+        this.error = null;
+      }, timeout);
+    }
   }
 }
