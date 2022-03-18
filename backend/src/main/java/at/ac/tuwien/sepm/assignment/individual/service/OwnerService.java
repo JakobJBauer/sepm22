70f731226331efbeb263e0277845f894277810f2
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2022-03-18 15:02:22 +0100

    [US9] Implemented get all owners functionality with filter and limit in service layer

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/service/OwnerService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/service/OwnerService.java
@@ -1,8 +1,13 @@
 package at.ac.tuwien.sepm.assignment.individual.service;
 
 import at.ac.tuwien.sepm.assignment.individual.entity.Owner;
+import at.ac.tuwien.sepm.assignment.individual.entity.OwnerSearchParams;
+
+import java.util.List;
 
 public interface OwnerService {
+    List<Owner> getAllOwners(OwnerSearchParams ownerSearchParams);
+
 
     Owner createOwner(Owner owner);
 }
--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/service/impl/OwnerServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/service/impl/OwnerServiceImpl.java
@@ -1,9 +1,12 @@
 package at.ac.tuwien.sepm.assignment.individual.service.impl;
 
 import at.ac.tuwien.sepm.assignment.individual.entity.Owner;
+import at.ac.tuwien.sepm.assignment.individual.entity.OwnerSearchParams;
 import at.ac.tuwien.sepm.assignment.individual.persistence.OwnerDao;
 import at.ac.tuwien.sepm.assignment.individual.service.OwnerService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OwnerServiceImpl implements OwnerService {
@@ -11,6 +14,11 @@ public class OwnerServiceImpl implements OwnerService {
 
     public OwnerServiceImpl(OwnerDao dao) {
         this.dao = dao;
+    }
+
+    @Override
+    public List<Owner> getAllOwners(OwnerSearchParams ownerSearchParams) {
+        return dao.getAllOwners(ownerSearchParams);
     }
 
     @Override
