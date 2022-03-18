31278fc4fb130fa8e28fd36796383d885b36a7ce
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2022-03-18 14:58:48 +0100

    [US8] Added create owner component and implemented gui

---
+++ frontend/src/app/component/owner-create/owner-create.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {Owner} from "../../dto/owner";
+import {Horse} from "../../dto/horse";
+import {OwnerService} from "../../service/owner.service";
+
+@Component({
+  selector: 'app-owner-create',
+  templateUrl: './owner-create.component.html',
+  styleUrls: ['./owner-create.component.scss']
+})
+export class OwnerCreateComponent implements OnInit {
+
+  owner: Owner;
+  savedSuccess = false;
+  error?: string;
+
+  constructor(
+    private service: OwnerService
+  ) { }
+
+  ngOnInit(): void {
+    this.owner = {} as Owner;
+  }
+
+  createOwner(): void {
+    this.service.create(this.owner).subscribe({
+      next: data => {
+        this.savedSuccess = true;
+        setTimeout(() => {
+          this.savedSuccess = false;
+        }, 5000);
+      },
+      error: error => {
+        this.error = error.message;
+        setTimeout(() => {
+          this.error = null;
+        }, 10000);
+      }
+    });
+  }
+}
