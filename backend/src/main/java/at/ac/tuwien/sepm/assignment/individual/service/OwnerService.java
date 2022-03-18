674449779dc918a73fe1891d4b9081ac09b625d3
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2022-03-18 14:36:50 +0100

    [US8] Added create owner functionality in service layer

---
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/service/OwnerService.java
@@ -0,0 +1,8 @@
+package at.ac.tuwien.sepm.assignment.individual.service;
+
+import at.ac.tuwien.sepm.assignment.individual.entity.Owner;
+
+public interface OwnerService {
+
+    Owner createOwner(Owner owner);
+}
