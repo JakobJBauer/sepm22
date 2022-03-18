a454981ca64d9a20df154a3a406dd7b5171b5f45
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2022-03-18 14:44:44 +0100

    [US8] Created owner service in frontend and added create owner functionality

---
+++ frontend/src/app/service/owner.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+import {Owner} from '../dto/owner';
+
+const baseUri = environment.backendUrl + '/owners';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OwnerService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  create(owner: Owner): Observable<Owner> {
+    return this.http.post<Owner>(baseUri, owner);
+  }
+
+}
