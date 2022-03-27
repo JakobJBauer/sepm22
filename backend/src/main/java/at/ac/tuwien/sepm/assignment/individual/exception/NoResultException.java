c075ee74d92e741a64f2f7553af8cbd7994855f9
JakobJBauer <53357351+JakobJBauer@users.noreply.github.com>
2022-03-27 21:35:28 +0200

    [TS27] Fixed Exception bug

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/exception/NoResultException.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/exception/NoResultException.java
@@ -4,6 +4,6 @@ package at.ac.tuwien.sepm.assignment.individual.exception;
  * NoResultException signals that an operation did not return any result
  * despite being expected to do so
  */
-public class NoResultException extends PersistenceException {
+public class NoResultException extends RuntimeException {
     public NoResultException(String message) { super(message); }
 }
